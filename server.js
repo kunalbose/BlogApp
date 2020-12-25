b12c1bc19722084c3e93ec383e44cb8acb319f2b
kunalbose <kunalbose234@gmail.com>
2020-12-25 14:41:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/articles')
+const app = express()
+const articleRouter = require('./routes/article')
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async(req, res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
+    res.render('articles/index', {articles: articles})
+})
+
+app.use('/article', articleRouter)
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => console.log(`listening on port ${port}`) )
